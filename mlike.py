d5be196a5ad4f176c0968bbcca8ba51b9ba599d4
Chamsseddin <98670814+Chamsseddin@users.noreply.github.com>
2022-06-10 03:17:51 +0100

    Update mlike.py

--- mlike.py
+++ mlike.py
@@ -1,14 +1,26 @@
 import requests
 
 class Client:
-    def __init__(self,phone,country_code='+213'):
+    
+    def __init__(self, phone, country_code = '+213'):
         self.phone = str(phone)
         self.country_code = country_code
         self.token = 'Bearer ' + self.get_token()
+    
+    def sign_up(self):
+        return requests.post('http://mlikeapiv21.yosarete.com:8092/api/user/create', headers = {
+            'content-type': 'application/x-www-form-urlencoded; charset=utf-8',
+            'accept-encoding': 'gzip'
+        }, data = {
+            'country_code': self.country_code,
+            'phone': self.phone,
+            'platform': 'android'
+        }).json()
+    
     def get_token(self):
-        token = lambda:requests.post('http://mlikeapiv21.yosarete.com:8092/oauth/token',headers = {
+        token = lambda : requests.post('http://mlikeapiv21.yosarete.com:8092/oauth/token', headers = {
                 'content-type': 'application/x-www-form-urlencoded; charset=utf-8'
-            },data = {
+            }, data = {
                 'client_id': '2',
                 'client_secret': 'LNY5G0PPCZChAORDV9bNcYwYFNfoBJlWY4hz6jVv',
                 'grant_type': 'password',
@@ -21,19 +33,15 @@ class Client:
             print('Create account with phone number' + self.phone)
             self.sign_up()
             return token()
-    def sign_up(self):
-        return requests.post('http://mlikeapiv21.yosarete.com:8092/api/user/create', headers = {
-            'content-type': 'application/x-www-form-urlencoded; charset=utf-8',
-            'accept-encoding': 'gzip'
-        }, data = {
-            'country_code': self.country_code,
-            'phone': self.phone,
-            'platform': 'android'
-        }).json()
+    
+    def update_token(self):
+        self.token = 'Bearer ' + self.get_token()
+    
     def get_credit(self):
-        return requests.post('http://mlikeapiv21.yosarete.com:8092/api/user/credit/get/all',headers = {
+        return requests.post('http://mlikeapiv21.yosarete.com:8092/api/user/credit/get/all', headers = {
             'authorization': self.token
         }).json()
+    
     def set_credit(self):
         return requests.post('http://mlikeapiv21.yosarete.com:8092/api/user/credit/set/ads',headers = {
             'authorization': self.token
